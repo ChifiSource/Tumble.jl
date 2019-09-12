3383b0c67ee3ce587c35df5384a62c8ede13953d
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-09-12 00:58:13 -0400

    Updated Stats library: Added dftools

--- Lathe.jl
+++ Lathe.jl
@@ -114,7 +114,7 @@ function f_test(sample,general)
     f =  sampvar / totvariance
 end
 #<----F-Value---->
-function f_value(array)
+function f_value(array
 
 end
 #-------Bayesian--------------___________
@@ -149,8 +149,33 @@ Preprocessing
      Module
 ================#
 module preprocess
-
-#----------------------------
+using Lathe.stats
+function TrainTest(data, at = 0.7)
+    n = nrow(data)
+    idx = shuffle(1:n)
+    train_idx = view(idx, 1:floor(Int, at*n))
+    test_idx = view(idx, (floor(Int, at*n)+1):n)
+    data[train_idx,:], data[test_idx,:]
+    return(train_idx,test_idx)
+end
+function StandardScalar(array)
+    standardized = 0
+    return(standardized)
+end
+#-----------------------------
+end
+#================
+Data
+    Tools
+================#
+module dftools
+function shake(array)
+    array = shuffle(array)
+end
+function matshuffle(mat, row)
+    mat[row,:] = shuffle(mat[row,:])
+end
+#-----------------------------
 end
 #================
 Predictive
