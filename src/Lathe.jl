0be10ff1de17f9606b1264380f773e8bb12d17a4
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-09-30 14:14:01 -0400

    Updated Sort Split, as well as Uniform Split

--- src/Lathe.jl
+++ src/Lathe.jl
@@ -209,11 +209,13 @@ function DfTrainTest(data, at = 0.7)
 end
 # Sort-------------
 function SortSplit(data,at=.25,reverse=false)
-    data = Array([data])
     idx = sort!(data)
     n = length(data)
-    data1,data2 = Uniform_Split(data,.75)
-    return(data1,data2)
+    idx = data
+    train_idx = view(idx, 1:floor(Int, at*n))
+    test_idx = view(idx, (floor(Int, at*n)+1):n)
+    data[train_idx,:], data[test_idx,:]
+    return(test_idx,train_idx)
 end
 # Unshuffled Split ----
 function Uniform_Split(data, at = 0.7)
