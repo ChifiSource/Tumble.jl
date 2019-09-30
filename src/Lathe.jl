07b4db78fa200daa4ffcd6e273fde2883196d45a
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-09-30 13:56:52 -0400

    Add files via upload

--- src/Lathe.jl
+++ src/Lathe.jl
@@ -191,7 +191,16 @@ Generalized
 ===============#
 # Train-Test-Split-----
 function TrainTest(data, at = 0.7)
-    n = Lathe.stats.nrow(data)
+    n = length(data)
+    idx = Random.shuffle(1:n)
+    train_idx = view(idx, 1:floor(Int, at*n))
+    test_idx = view(idx, (floor(Int, at*n)+1):n)
+    data[train_idx,:], data[test_idx,:]
+    return(test_idx,train_idx)
+end
+# DataFrames TestTrainSplit -----
+function DfTrainTest(data, at = 0.7)
+    n = nrow(data)
     idx = Random.shuffle(1:n)
     train_idx = view(idx, 1:floor(Int, at*n))
     test_idx = view(idx, (floor(Int, at*n)+1):n)
