b45fae326bcd2d003d07dd2be75c72adc40db2fa
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-10-10 01:47:49 -0400

    Add files via upload

--- src/Lathe.jl
+++ src/Lathe.jl
@@ -10,8 +10,12 @@ GNU General Open Source License
 Thank you for your forks!
 <-----------Lathe.jl----------->
 38d8eb38-e7b1-11e9-0012-376b6c802672
+#[deps]
+DataFrames.jl
+Random.jl
 ================================#
 module Lathe
+using DataFrames
 #================
 Stats
     Module
@@ -45,9 +49,14 @@ function variance(array)
 end
 #<----Standard Deviation---->
 function std(array)
-    mean = sum(array)/length(array)
-    sq = sum(array) - mean
-    squared_mean = sq ^ 2
+    mean = mean(array)
+    l = []
+    for i in array
+        subtr = (i - mean) ^ 2
+        append!(l,subtr)
+    end
+    mean = mean(l)
+    squared_mean = l ^ 2
     standardized = sqrt(squared_mean)
     return(standardized)
 end
@@ -188,9 +197,28 @@ end
 function wilcoxsr(var1,var2)
 
 end
+#<---- Binomial Distribution ---->
+function binomialdist(positives,negatives,length)
+    # p = n! / x!(n-x!)*π^x*(1-π)^N-x
+end
 #<---- Sign Test ---->
 function sign(var1,var2)
-
+    sets = var1 .- var2
+    positives = []
+    negatives = []
+    zeros = []
+    for i in sets
+        if i == 0
+            zeros.append(i)
+        elseif i > 0
+            positives.append(i)
+        elseif i < 0
+            negatives.append(i)
+    end
+    totalnegs = length(negatives)
+    totalpos = length(positives)
+    totalzer = length(zeros)
+    totallen = length(sets)
 end
 #<---- F-Test---->
 function f_test(sample,general)
@@ -355,8 +383,7 @@ function StandardScalar(array)
     avg = Lathe.stats.mean(array)
     v = []
     for i in array
-        x = i
-        y = (x-avg) / q
+        y = (i-avg) / q
         append!(v,y)
     end
     return(v)
@@ -466,80 +493,61 @@ function pred_multigap(m,xt)
 
 end
 #==
-Quad
-    Range
+Four
+    Square
 ==#
 # Model Type
 mutable struct FourSquare
     x
     y
+    n_divisions = 4
+    divisionsize = .25
 end
 #----  Callback
 function pred_foursquare(m,xt)
 # x = q1(r(floor:q1)) |x2 = q2(r(q1:μ)) |x3 = q3(r(q2:q3)) |x4 q4(r(q3:cieling))
 # y' = q1(x * (a / x)) | μ(x * (a / x2)) | q3(x * (a / x3) | q4(x * (a / x4))
+# Original 4 quartile math ^^
     x = m.x
     y = m.y
+    xtcopy = xt
+    divs = m.n_divisions
+    size = m.divisionsize
     # Go ahead and throw an error for the wrong input shape:
     xlength = length(x)
     ylength = length(y)
     if xlength != ylength
         throw(ArgumentError("The array shape does not match!"))
     end
-    # Our empty Y prediction list==
-    e = []
-    # Quad Splitting the data ---->
-    # Split the Y
-    y2,range1 = Lathe.preprocess.SortSplit(y)
-    y3,range2 = Lathe.preprocess.SortSplit(y2)
-    y4,range3 = Lathe.preprocess.SortSplit(y3)
-    range4 = y4
-    # Split the x train
-    x1,xrange1 = Lathe.preprocess.SortSplit(x)
-    x2,xrange2 = Lathe.preprocess.SortSplit(x1)
-    x3,xrange3 = Lathe.preprocess.SortSplit(x2)
-    xrange4 = x3
-    # Fitting the 4 linear regression models ---->
-    regone = LinearRegression(xrange1,range1)
-    regtwo = LinearRegression(xrange2,range2)
-    regthree = LinearRegression(xrange3,range3)
-    regfour = LinearRegression(xrange4,range4)
-    # Split the train Data
-    xt1,xtrange1 = Lathe.preprocess.SortSplit(xt)
-    xt2,xtrange2 = Lathe.preprocess.SortSplit(xt1)
-    xt3,xtrange3 = Lathe.preprocess.SortSplit(xt2)
-    xtrange4 = xt3
-    # Get min-max
-    xtrange1min = minimum(xtrange1)
-    xtrange1max = maximum(xtrange1)
-    xtrange2min = minimum(xtrange2)
-    xtrange2max = maximum(xtrange2)
-    xtrange3min = minimum(xtrange3)
-    xtrange3max = maximum(xtrange3)
-    xtrange4min = minimum(xtrange4)
-    xtrange4max = maximum(xtrange4)
-    # Ranges for ifs
-    condrange1 = (xtrange1min:xtrange1max)
-    condrange2 = (xtrange2min:xtrange2max)
-    condrange3 = (xtrange3min:xtrange3max)
-    condrange4 = (xtrange4min:xtrange4max)
-    # This for loop is where the dimension's are actually used:
-    for i in xt
-        if i in condrange1
-            ypred = predict(regone,i)
-        end
-        if i in condrange2
-            ypred = predict(regtwo,i)
-        end
-        if i in condrange3
-            ypred = predict(regthree,i)
-        end
-        if i in condrange4
-            ypred = predict(regfour,i)
-        end
-        append!(e,ypred)
+    # Now we also need an error for when the total output of the
+    #    division size and n divisions is > 100 percent
+    divisions = size * div
+    if divisions > 1
+        throw(ArgumentError("Invalid hyperparameters!: divisions * number of
+        divisions must be = to 100 percent!"))
+    elseif divisions < 1
+        throw(ArgumentError("Invalid hyperparameters!: divisions * number of
+        divisions must be = to 100 percent!"))
     end
-    return(e)
+    # Put our n/divisions into a range
+    ran = (1:divs)
+    # Empty list
+    e = []
+    # Now for sort-split and predicting:
+    for i in ran
+        x,predictorx = SortSplit(x,divisionsize)
+        y,predictory = SortSplit(y,divisionsize)
+        xtcopy,predictorxt = SortSplit(xtcopy,divisionsize)
+        currentrange = (minimum(predictorxt):maximum(xtcopypredictorxt)
+        linregmod = LinearRegression(predictorx,predictory)
+        for i in xt
+            if i in currentrange
+                ypred = predict(linregmod,i)
+                append!(predictionlist,ypred)
+            end
+        end
+    end
+    return(xt)
 end
 #==
 Linear
@@ -681,6 +689,7 @@ Pipeline
     Module
 ================#
 module Pipelines
+#=================================================
 #
 # Note to future self, or other programmer:
 # It is not necessary to store these as constructors!
@@ -736,7 +745,7 @@ function predict(fitpipeline,xt)
         ypr = Lathe.models.predict(model,xt)
     end
 end
-#
+=============================================#
 #----------------------------------------------
 end
 #==
