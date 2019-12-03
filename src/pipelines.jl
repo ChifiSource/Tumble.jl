34cdd616797e19d6fad931800d0d140b7320c3a2
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-12-03 18:54:25 -0500

    Add files via upload

--- src/pipelines.jl
+++ src/pipelines.jl
@@ -10,13 +10,19 @@ end
 function predict(pipe,xt)
     fx = []
     m = pipe.model
-    for i in pipe.steps
-        u = i(m.x)
+    for step in pipe.steps
+        u = step(m.x)
         append!(fx,u)
     end
     model.x = fx
     pr = Lathe.models.predict(model,xt)
     return(pr)
+end
+function serialize(pip,uri)
+
+end
+function deserialize(pip,uri)
+
 end
 #------------------
 end
