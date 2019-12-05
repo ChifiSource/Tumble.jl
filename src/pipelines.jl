95dfbd36cd152b40c36bd29a52b254c3671d20c5
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-12-05 16:55:37 -0500

    Added deprecation warnings

--- src/pipelines.jl
+++ src/pipelines.jl
@@ -8,6 +8,12 @@ mutable struct Pipeline
     model
 end
 function pippredict(pipe,xt)
+    println("---DEPRECATION WARNING---")
+    println("Lathe.pipelines is set to be deprecated in Lathe 0.1.0")
+    println("! Use Pipeline([steps],model) from Lathe.models")
+    println("julia> using Lathe.models: Pipeline, predict")
+    println("pipe = Pipeline([StandardScalar],LinearRegression(StandardScalar(trainX),trainy))
+    println("predict(pipe,Xtrain)")
     for step in pipe.steps
         xt = step(xt)
     end
