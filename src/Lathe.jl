49e146a72ed52b52d8735ebc67fc69b8dbd5e790
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-12-13 17:09:32 -0600

    Added docstring tree back

--- src/Lathe.jl
+++ src/Lathe.jl
@@ -14,7 +14,22 @@ Thank you for your forks!
 DataFrames.jl
 Random.jl
 ================================#
+@doc """
+      Lathe 0.0.8 - Easily ML \n
+      ================= \n
+      |\n
+      |_____Lathe.stats\n
+      |_____Lathe.validate\n
+      |_____Lathe.preprocess\n
+      |_____Lathe.models\n
+      -----------------\n
+      You can use ? on any of these four modules to see the functions inside of them\n
+      \n
+      Lathe is an all-in-one inclusive package for data-science, statistics, and
+      machine-learning inside of Julia. For more information, you can see our
+      home over at http://emmettboudreau.com/Lathe
 
+       """ ->
 module Lathe
 
 # <------- PARTS ----->
