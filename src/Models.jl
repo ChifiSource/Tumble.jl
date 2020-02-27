464ebd58885de63151b3337e4f08d2ad68a9b4eb
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-02-27 15:57:52 -0500

    Added model training, (No predicting)

--- src/Models.jl
+++ src/Models.jl
@@ -329,32 +329,22 @@ Nueral
     Network
         Framework
 ==#
-
 function calculate_activation_forward(A_pre , W , b , function_type)
-
     if (function_type == "sigmoid")
-
         Z , linear_step_cache = forward_linear(A_pre , W , b)
         A , activation_step_cache = sigmoid(Z)
-
     elseif (function_type == "relu")
-
         Z , linear_step_cache = forward_linear(A_pre , W , b)
         A , activation_step_cache = relu(Z)
-
     end
-
     cache = (linear_step_cache , activation_step_cache)
     return A , cache
-
 end
 
 function model_forward_step(X , params)
-
     all_caches = []
     A = X
     L = length(params)/2
-
     for l=1:L-1
         A_pre = A
         A , cache = calculate_activation_forward(A_pre , params[string("W_" , string(Int(l)))] , params[string("b_" , string(Int(l)))] , "relu")
@@ -362,53 +352,34 @@ function model_forward_step(X , params)
     end
     A_l , cache = calculate_activation_forward(A , params[string("W_" , string(Int(L)))] , params[string("b_" , string(Int(L)))] , "sigmoid")
     push!(all_caches , cache)
-
-
     return A_l , all_caches
-
 end
 function cost_function(AL , Y)
-
     cost = -mean(Y.*log.(AL) + (1 .- Y).*log.(1 .- AL))
-
     return cost
-
 end
 function forward_linear(A , w , b)
-
     Z = w*A .+ b
     cache = (A , w , b)
-
     return Z,cache
-
 end
 function init_param(layer_dimensions)
-
     param = Dict()
-
     for l=2:length(layer_dimensions)
-
         param[string("W_" , string(l-1))] = rand(layer_dimensions[l] ,
 				layer_dimensions[l-1])*0.1
         param[string("b_" , string(l-1))] = zeros(layer_dimensions[l] , 1)
-
     end
-
     return param
-
 end
 
 function backward_linear_step(dZ , cache)
-
     A_prev , W , b = cache
-
     m = size(A_prev)[2]
-
     dW = dZ * (A_prev')/m
     db = sum(dZ , dims = 2)/m
     dA_prev = (W')* dZ
     return dW , db , dA_prev
-
 end
 function sigmoid(X)
     sigma = 1 ./(1 .+ exp.(.-X))
@@ -418,41 +389,28 @@ function relu(X)
     rel = max.(0,X)
     return rel , X
 end
-
 function backward_relu(dA , cache_activation)
     return dA.*(cache_activation.>0)
 end
-
 function backward_sigmoid(dA , cache_activation)
     return dA.*(sigmoid(cache_activation)[1].*(1 .- sigmoid(cache_activation)[1]))
 end
-
 function backward_activation_step(dA , cache , activation)
-
     linear_cache , cache_activation = cache
     if (activation == "relu")
-
         dZ = backward_relu(dA , cache_activation)
         dW , db , dA_prev = backward_linear_step(dZ , linear_cache)
-
     elseif (activation == "sigmoid")
-
         dZ = backward_sigmoid(dA , cache_activation)
         dW , db , dA_prev = backward_linear_step(dZ , linear_cache)
-
     end
-
     return dW , db , dA_prev
 
 end
 function model_backwards_step(A_l , Y , caches)
-
     grads = Dict()
-
     L = length(caches)
-
     m = size(A_l)[2]
-
     Y = reshape(Y , size(A_l))
     dA_l = (-(Y./A_l) .+ ((1 .- Y)./( 1 .- A_l)))
     current_cache = caches[L]
@@ -462,26 +420,18 @@ function model_backwards_step(A_l , Y , caches)
         grads[string("dW_" , string(l+1))] , grads[string("db_" , string(l+1))] , grads[string("dA_" , string(l))] = backward_activation_step(grads[string("dA_" , string(l+1))] , current_cache , "relu")
 
     end
-
     return grads
-
 end
 
 function update_param(parameters , grads , learning_rate)
-
     L = Int(length(parameters)/2)
-
     for l=0:(L-1)
-
         parameters[string("W_" , string(l+1))] -= learning_rate.*grads[string("dW_" , string(l+1))]
         parameters[string("b_",string(l+1))] -= learning_rate.*grads[string("db_",string(l+1))]
-
     end
-
     return parameters
-
 end
-function Network(x,y,chains)
+function Network(X,Y,layers_dimensions)
     params = init_param(layers_dimensions)
     costs = []
     iters = []
@@ -501,7 +451,6 @@ function Network(x,y,chains)
         predict(xt) = (xt + 4)
         (test)->(costs;params;predict)
     end
-
 end
 #
 predict(m::Pipeline,x) = pred_pipeline(m,x)
