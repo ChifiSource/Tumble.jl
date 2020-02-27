fb99f064d09d6f5561a0aab08c0bd8e8c9f704d1
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-02-27 15:51:08 -0500

    Backstep_Linear => Lathe.models (NNF)

--- src/Models.jl
+++ src/Models.jl
@@ -329,6 +329,99 @@ Nueral
     Network
         Framework
 ==#
+
+function calculate_activation_forward(A_pre , W , b , function_type)
+
+    if (function_type == "sigmoid")
+
+        Z , linear_step_cache = forward_linear(A_pre , W , b)
+        A , activation_step_cache = sigmoid(Z)
+
+    elseif (function_type == "relu")
+
+        Z , linear_step_cache = forward_linear(A_pre , W , b)
+        A , activation_step_cache = relu(Z)
+
+    end
+
+    cache = (linear_step_cache , activation_step_cache)
+    return A , cache
+
+end
+
+function model_forward_step(X , params)
+
+    all_caches = []
+    A = X
+    L = length(params)/2
+
+    for l=1:L-1
+        A_pre = A
+        A , cache = calculate_activation_forward(A_pre , params[string("W_" , string(Int(l)))] , params[string("b_" , string(Int(l)))] , "relu")
+        push!(all_caches , cache)
+    end
+    A_l , cache = calculate_activation_forward(A , params[string("W_" , string(Int(L)))] , params[string("b_" , string(Int(L)))] , "sigmoid")
+    push!(all_caches , cache)
+
+
+    return A_l , all_caches
+
+end
+function cost_function(AL , Y)
+
+    cost = -mean(Y.*log.(AL) + (1 .- Y).*log.(1 .- AL))
+
+    return cost
+
+end
+function forward_linear(A , w , b)
+
+    Z = w*A .+ b
+    cache = (A , w , b)
+
+    return Z,cache
+
+end
+function init_param(layer_dimensions)
+
+    param = Dict()
+
+    for l=2:length(layer_dimensions)
+
+        param[string("W_" , string(l-1))] = rand(layer_dimensions[l] ,
+				layer_dimensions[l-1])*0.1
+        param[string("b_" , string(l-1))] = zeros(layer_dimensions[l] , 1)
+
+    end
+
+    return param
+
+end
+
+function backward_linear_step(dZ , cache)
+
+    A_prev , W , b = cache
+
+    m = size(A_prev)[2]
+
+    dW = dZ * (A_prev')/m
+    db = sum(dZ , dims = 2)/m
+    dA_prev = (W')* dZ
+    return dW , db , dA_prev
+
+end
+function sigmoid(X)
+    sigma = 1 ./(1 .+ exp.(.-X))
+    return sigma , X
+end
+function relu(X)
+    rel = max.(0,X)
+    return rel , X
+end
+function Network(x,y,chains)
+
+end
 #
+predict(m::Pipeline,x) = pred_pipeline(m,x)
 #----------------------------------------------
 end
