5407c1ab5675325fd6bd2b13132b24eb505c5e55
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-05-24 04:36:37 -0400

    added models macros

--- src/models/model_macros.jl
+++ src/models/model_macros.jl
@@ -1,1 +1,7 @@
-# Null
+macro predict(m,trainx,trainy,xt)
+    expression = string(m,"(", trainx, ",", trainy, ")")
+    exp = Meta.parse(expression)
+    model = eval(exp)
+    xt = eval(xt)
+    pred = model.predict(xt)
+end
