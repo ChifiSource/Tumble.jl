a672c0a2039114d1812ad887857a330c8ae0d94e
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-05-24 04:37:21 -0400

    added preprocess macros

--- src/preprocess/preprocess_macros.jl
+++ src/preprocess/preprocess_macros.jl
@@ -1,1 +1,12 @@
-# Null
+macro tts(x,y,at=.75)
+    x = eval(x)
+    y = eval(y)
+    TrainTestSplit(x,y,at)
+end
+macro onehot(df,symb)
+    OneHotEncode(df,symb)
+end
+macro norm(x)
+    x = eval(x)
+    Normalizer(x).predict(x)
+end
