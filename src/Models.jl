e4d7a6e3d0fded95fda0af581a7becf0fba7591e
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-10-12 23:04:19 -0500

    fixed expression error

--- src/Models.jl
+++ src/Models.jl
@@ -7,14 +7,18 @@ Predictive
       |====== Lathe.models =====\n
       |____________/ Accessories ___________\n
       |_____models.Pipeline([steps])\n
+      |_____models.Powerlog(p1, p2)\n
       |____________/ Continuous models ___________\n
       |_____models.meanBaseline(y)\n
-      |_____models.LinearRegression(x,y)\n
+      |_____models.SimpleLinearRegression(x,y)\n
       |_____models.LeastSquare(x,y,Type)\n
       |_____models.PowerLog(prob1,prob2)\n
       |____________/ Categorical Models ___________\n
       |_____models.LogisticRegression(x,y)\n
       |_____models.majBaseline(y)\n
+      |____________/ Unsupervised Learning Models ___________\n
+      |_____models.Kmeans(k)\n
+
        """
 module models
 # [deps]
@@ -29,8 +33,8 @@ Continuous Models
 include("models/lsq.jl")
 export LeastSquare
 # Linear Regression
-include("models/regressors")
-export LinearRegression,
+include("models/linreg")
+export SimpleLinearRegression
 #==
 Categorical Models
 ==#
