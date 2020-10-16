8fec9ee6e33b3c21d0d65e8ca8f84fba8d7dfcbb
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-10-16 07:33:03 -0400

    Treeclass fix

--- src/models/treeclass.jl
+++ src/models/treeclass.jl
@@ -51,7 +51,7 @@ function DecisionTreeClassifier(X, Y, rng = Random.GLOBAL_RNG; max_depth = 6,
      min_node_records = 1,
     n_features_per_node = Int(floor(sqrt(size(X, 2)))), n_trees = 100)
     storedata = fit(TREECLASS(), X, Y, rng, max_depth, min_node_records,
-    n_features_per_node, n_trees)
+            n_features_per_node, n_trees)
     predict(xt) = predict(storedata, xt)
     (var)->(predict;storedata)
 end
