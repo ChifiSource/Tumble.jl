7c44c0b6e057cc11411319388501f4a60255c056
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-02-14 15:46:38 -0500

    added tests for SVD; Onehot

--- test/src/preprocesstests.jl
+++ test/src/preprocesstests.jl
@@ -1,15 +1,27 @@
-using Lathe.preprocess: SVD, OneHotEncoder
+using LinearAlgebra
+using DataFrames
+using Test
+println("LATHE.PREPROCESS TESTS")
+println("......................")
+# DECOMPOSITION
+println("DECOMPOSITION")
+println(".............")
+using Lathe.preprocess: SVD
 @testset "svd test for $T" for T in (Float32, Float64)
   A = randn(T, 100, 100)
   x = SVD(A)
-
   tol = T == Float32 ? 1e-5 : 1e-10
   @test (norm(A - (x.U * diagm(0 => x.S) * x.V'), 2) / norm(A, 2)) <= tol
 end
-
-df = DataFrame(:A => ["A", "C", "A"], :B => [5,10,15])
-
-@testset "Encoder Tests" begin
+using Lathe.preprocess: OneHotEncoder, FloatEncoder, OrdinalEncoder
+# ENCODING
+df = DataFrame(:A => [5, 10, 15, 20], :B => ["test", "test2", "test2", "test"])
+println("ENCODING")
+println("........")
+@testset "OneHotEncoder" begin
 encoder = OneHotEncoder()
-@test encoder.predict(df)
+ant = size(df)[2]
+data = encoder.predict(df, :A)
+len = Set(df[!, :A])
+@test size(data)[2] - ant == length(len)
 end
--- test/src/test.jl
+++ test/src/test.jl
@@ -1,4 +1,3 @@
-
 using Test
 using LinearAlgebra
 using DataFrames
