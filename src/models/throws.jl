e4de4a7fdc899b84a37714ed47d070bcef5ad3ac
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-05-08 18:44:03 -0400

    added new throws and cuda call

---
+++ src/models/throws.jl
@@ -0,0 +1,12 @@
+using CUDA
+function checkdims(x::AbstractArray, y::AbstractArray)
+    @assert length(x) == length(Array(y)) DimensionMismatch("Shape does not match!")
+end
+function cudacheck(arrays::Array{Array}, cuda::Bool)
+    if cuda == true
+        new = [x = CuArray(x) for x in arrays]
+        return(new)
+    else
+        return(arrays)
+    end
+end
